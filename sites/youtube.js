d0dcd290c003454520d48e23b4f344f641be1dfb
ConorTheDev <con0r.b@protonmail.com>
2019-09-23 20:37:13 +0100

    :bug: Fix video id parsing on YouTube

--- sites/youtube.js
+++ sites/youtube.js
@@ -1,56 +1,61 @@
 setInterval(() => {
-    let title = $("h1")
-        .filter(".title")
-        .filter(".style-scope")
-        .filter(".ytd-video-primary-info-renderer")
-        .text();
-    let artist = $("#upload-info .ytd-channel-name")[0].innerText;
-    let albumart =
-        "https://img.youtube.com/vi/" +
-        $("iframe:first")[0].baseURI.replace(
-            "https://www.youtube.com/watch?v=",
-            ""
-        ) +
-        "/0.jpg";
+  function youtube_parser(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return match && match[7].length == 11 ? match[7] : false;
+  }
 
-    let timestampSeconds = Math.floor($(".video-stream")[0].currentTime);
-    let lengthSeconds = Math.floor($(".video-stream")[0].duration);
-    let timestamp = timestampSeconds * 1000;
-    let length = lengthSeconds * 1000;
+  let title = $("h1")
+    .filter(".title")
+    .filter(".style-scope")
+    .filter(".ytd-video-primary-info-renderer")
+    .text();
+  let artist = $("#upload-info .ytd-channel-name")[0].innerText;
+  let albumart =
+    "https://img.youtube.com/vi/" +
+    youtube_parser($("iframe:first")[0].baseURI) +
+    "/0.jpg";
 
-    if (title === "") {
-        return;
+  console.log(albumart);
+
+  let timestampSeconds = Math.floor($(".video-stream")[0].currentTime);
+  let lengthSeconds = Math.floor($(".video-stream")[0].duration);
+  let timestamp = timestampSeconds * 1000;
+  let length = lengthSeconds * 1000;
+
+  if (title === "") {
+    return;
+  }
+
+  let data = {
+    progress_ms: timestamp,
+    item: {
+      album: {
+        artists: [
+          {
+            name: artist
+          }
+        ],
+        images: [
+          {
+            url: albumart
+          }
+        ]
+      },
+      duration_ms: length,
+      name: title
     }
+  };
 
-    let data = {
-        progress_ms: timestamp,
-        item: {
-            album: {
-                artists: [
-                    {
-                        name: artist
-                    }
-                ],
-                images: [
-                    {
-                        url: albumart
-                    }
-                ]
-            },
-            duration_ms: length,
-            name: title
-        }
-    };
-
-    fetch("http://localhost:9102/", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => console.log(response))
-        .catch(error =>
-            console.error("[MediaMod] Error when sending request", error)
-        );
+  fetch("http://localhost:9102/", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => console.log(response))
+    .catch(error =>
+      console.error("[MediaMod] Error when sending request", error)
+    );
 }, 3000);
